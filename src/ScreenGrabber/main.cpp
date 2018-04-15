2eb1ace0940c752e8ab66781ab4ccce109d16497
pyarovoy@y.y <pyarovoy@y.y>
2018-04-15 10:19:02 +0300

    Added submodules

--- src/ScreenGrabber/main.cpp
+++ src/ScreenGrabber/main.cpp
@@ -13,23 +13,6 @@ int main(int argc, char *argv[])
 	QCoreApplication::setOrganizationName("NTT");
 	QCoreApplication::setApplicationName("ZavScreenGrabber");
 
-	QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
-	QLocale::setDefault(QLocale("ru_RU"));
-
-	QTranslator translatorScreenGrabber;
-	QTranslator translatorScreenGrabberCore;
-
-	// init language files
-	QString language = "ru";
-
-	//Q_INIT_RESOURCE(ScreenGrabber_ts);
-	//Q_INIT_RESOURCE(ScreenGrabberCore_ts);
-
-	translatorScreenGrabber.load(QString(":/ScreenGrabber_%1.qm").arg(language));
-	a.installTranslator(&translatorScreenGrabber);
-	translatorScreenGrabberCore.load(QString(":/ScreenGrabberCore_%1.qm").arg(language));
-	a.installTranslator(&translatorScreenGrabberCore);
-
 	MainWindow* view = new MainWindow();
 	view->showMaximized();
 	view->startTo();
